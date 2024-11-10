35627b5052bdfce1c3284c73e7a2315d080ad48b
ntkiet1912 <pupgkiet@gmail.com>
2024-11-10 17:38:08 +0700

    add create employee UI

---
+++ BookstoreManagement/UI/EmployeeUI/CreateEmployeeV.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace BookstoreManagement.UI.EmployeeUI;
+
+public partial class CreateEmployeeV : UserControl
+{
+    public CreateEmployeeV()
+    {
+        InitializeComponent();
+    }
+}
---
+++ BookstoreManagement/UI/EmployeeUI/CreateEmployeeVM.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using BookstoreManagement.Core;
+using BookstoreManagement.DbContexts;
+using BookstoreManagement.Models;
+using BookstoreManagement.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BookstoreManagement.UI.EmployeeUI;
+
+public partial class CreateEmployeeVM : BaseViewModel
+{
+    private readonly ApplicationDbContext Db;
+    private readonly INavigatorService<AllEmployeeVM> allEmployeeNavigator;
+
+    [ObservableProperty]
+    private string _firstName;
+    [ObservableProperty] 
+    private string _lastName;
+    [ObservableProperty]
+    private string _email;
+    [ObservableProperty]
+    private decimal _salary;
+    [ObservableProperty]
+    private bool _isManager;
+
+    public CreateEmployeeVM(ApplicationDbContext db , INavigatorService<AllEmployeeVM> allemployeeNavigator)
+    {
+        Db = db;
+        allEmployeeNavigator = allemployeeNavigator;
+    }
+
+    [RelayCommand]
+    private void Submit()
+    {
+        var item = new Employee
+        {
+            FirstName = FirstName,
+            LastName = LastName,
+            Email = Email,  
+            Salary = Salary,
+            IsManager = IsManager
+        };
+        try
+        {
+            Db.Add(item);
+            Db.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Couldn't add item! {ex}" , "Error" , MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+        MessageBox.Show("Added item successfully!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+}
