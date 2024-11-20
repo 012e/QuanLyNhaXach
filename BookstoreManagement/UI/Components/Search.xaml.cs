e123c9b2eaa0d4727844fb1d7bc93f3ef8640da2
Pham Nhat Huy <huyphmnhat@gmail.com>
2024-11-20 08:14:10 +0700

    make search box a custom component

---
+++ BookstoreManagement/UI/Components/Search.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookstoreManagement.UI.Components
+{
+    /// <summary>
+    /// Interaction logic for Search.xaml
+    /// </summary>
+    public partial class Search : UserControl
+    {
+        public String Text
+        {
+            get { return (String)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(MyCommandProperty); }
+            set { SetValue(MyCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty MyCommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(Search), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(String), typeof(Search), new PropertyMetadata(""));
+
+        public String PlaceHolder
+        {
+            get { return (String)GetValue(PlaceHolderProperty); }
+            set { SetValue(PlaceHolderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PlaceHolder.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlaceHolderProperty =
+            DependencyProperty.Register("PlaceHolder", typeof(String), typeof(Search), new PropertyMetadata(""));
+
+        public Search()
+        {
+            InitializeComponent();
+        }
+    }
+}
